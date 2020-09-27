502ba7f2e80b8082da322843aacfe408009b0c25
kapiljhajhria <jhajhria44@gmail.com>
2020-09-27 19:00:48 +0530

    course schema added

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const mongoose = require('mongoose');
 
 
-mongoose.connect('mongodb://localhost:27017/playground', {useNewUrlParser: true,useUnifiedTopology: true})
-    .then(()=>console.log('connected to MongoDb Database....'))
-    .catch(error=>console.log('could not connect to MongoDb, error is:'+error));
+mongoose.connect('mongodb://localhost:27017/playground', {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log('connected to MongoDb Database....'))
+    .catch(error => console.log('could not connect to MongoDb, error is:' + error));
+
+
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tag: [String],
+    date: {
+        type: Date, default: Date.now(),
+        isPublished: Boolean,
+    }
+});
